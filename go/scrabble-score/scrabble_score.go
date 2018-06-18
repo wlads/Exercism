c7f2f8e6f4c1a104444a8261bdb236ed1237c176
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2018-06-18 18:40:03 -0300

    Fix formating

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -13,6 +13,7 @@ func Score(str string) int {
   return sum
 }
 
+// convertScore is case insensitive
 // Letter                           Value
 // A, E, I, O, U, L, N, R, S, T       1
 // D, G                               2
@@ -23,21 +24,21 @@ func Score(str string) int {
 // Q, Z                               10
 func convertScore(r rune) int {
 	switch(unicode.ToUpper(r)) {
-		case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
-			return 1
-		case 'D', 'G':
-			return 2
-		case 'B', 'C', 'M', 'P':
-			return 3
-		case 'F', 'H', 'V', 'W', 'Y':
-			return 4
-		case 'K':
-			return 5
-		case 'J', 'X':
-			return 8
-		case 'Q', 'Z':
-			return 10
-		default:
-			return 0
-		}
+	case 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T':
+		return 1
+	case 'D', 'G':
+		return 2
+	case 'B', 'C', 'M', 'P':
+		return 3
+	case 'F', 'H', 'V', 'W', 'Y':
+		return 4
+	case 'K':
+		return 5
+	case 'J', 'X':
+		return 8
+	case 'Q', 'Z':
+		return 10
+	default:
+		return 0
+	}
 }
