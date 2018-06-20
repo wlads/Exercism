acd17a12ec6f3537bd0ce365c2cd3c57fb844dd5
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2018-06-20 10:56:47 -0300

    New solution usign bits

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,12 +2,11 @@ package grains
 
 import (
 	"errors"
-	"math"
 )
 
 // Total number of wheat grains in a chessboard: 1, 2, 4, 8, ...
 func Total() uint64 {
-	return 18446744073709551615
+	return 1<<64 - 1
 }
 
 // Square calculates the number of wheat grains in a position (pos)
@@ -15,9 +14,5 @@ func Square(pos int) (uint64, error) {
 	if pos < 1 || pos > 64 {
 		return 0, errors.New("position must be between 1 and 64")
 	}
-	grains := uint64(0)
-	for i := 0; i < pos; i++ {
-		grains = uint64(math.Pow(2, float64(i)))
-	}
-	return grains, nil
+	return 1 << uint(pos-1), nil
 }
